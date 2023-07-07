35028b419b299ecd4c64515a5dd78a00af552f28
SpeedyGo55 <luca.buholzer@eccom.ch>
2023-07-07 20:29:54 +0200

    Added functions to CardStack class.

--- PythonCards.py
+++ PythonCards.py
@@ -15,17 +15,17 @@ class CardStack:
     def __repr__(self):
         return str(self.cards_on_stack)
 
+    def __len__(self):
+        count = 0
+        for suit in self.cards_on_stack:
+            count += len(self.cards_on_stack[suit])
+        return count
+
     def get_cards_in_play(self):
         return self.cards_in_play
 
     def get_cards_on_stack(self):
         return self.cards_on_stack
-
-    def get_number_of_cards_on_stack(self):
-        count = 0
-        for suit in self.cards_on_stack:
-            count += len(self.cards_on_stack[suit])
-        return count
 
     def get_number_of_cards_in_play(self):
         count = 0
@@ -36,7 +36,7 @@ class CardStack:
         output = []
         if amount > 52 or amount < 0:
             raise ValueError("Error: Amount is not between 0 and 52")
-        if amount > self.get_number_of_cards_on_stack():
+        if amount > len(self):
             raise ValueError("Error: Not enough cards on stack")
         for i in range(amount):
             suit = random.choice(list(self.cards_on_stack.keys()))
@@ -50,6 +50,41 @@ class CardStack:
             self.cards_on_stack[suit].remove(card)
             output.append(tuple([suit, card]))
         return output
+    def put_card_back_on_stack(self, card):
+        if type(card) != tuple:
+            raise TypeError("Error: Card is not a tuple")
+        if len(card) != 2:
+            raise ValueError("Error: Card tuple does not have 2 values")
+        if card[0] not in self.cards_in_play:
+            raise ValueError("Error: Suit is not valid")
+        if card[1] not in self.cards_in_play[card[0]]:
+            raise ValueError("Error: Card is not valid")
+        self.cards_on_stack[card[0]].append(card[1])
+        self.cards_in_play[card[0]].remove(card[1])
+
+    def put_hand_back_on_stack(self, hand):
+        if type(hand) != list:
+            raise TypeError("Error: Hand is not a list")
+        for card in hand:
+            self.put_card_back_on_stack(card)
+    def remove_card_from_stack(self, card):
+        if type(card) != tuple:
+            raise TypeError("Error: Card is not a tuple")
+        if len(card) != 2:
+            raise ValueError("Error: Card tuple does not have 2 values")
+        if card[0] not in self.cards_on_stack:
+            raise ValueError("Error: Suit is not valid")
+        if card[1] not in self.cards_on_stack[card[0]]:
+            raise ValueError("Error: Card is not valid")
+        self.cards_on_stack[card[0]].remove(card[1])
+
+    def remove_cards_from_stack(self, cards):
+        if type(cards) != list:
+            raise TypeError("Error: Cards is not a list")
+        for card in cards:
+            remove_card_from_stack(card)
+
+
 
 
 
