2a81528901e0802792c1183a748c1f53922220ba
SpeedyGo55 <luca.buholzer@eccom.ch>
2023-07-08 21:09:06 +0200

    Added LICENSE

--- src/PythonCards.py
+++ src/PythonCards.py
@@ -1,5 +1,6 @@
 import random
 
+
 class CardStack:
     def __init__(self):
         self.cards_on_stack = {"Spades": ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"],
@@ -32,6 +33,7 @@ class CardStack:
         for suit in self.cards_in_play:
             count += len(self.cards_in_play[suit])
         return count
+
     def draw(self, amount):
         output = []
         if amount > 52 or amount < 0:
@@ -50,6 +52,7 @@ class CardStack:
             self.cards_on_stack[suit].remove(card)
             output.append(tuple([suit, card]))
         return output
+
     def put_card_back_on_stack(self, card):
         if type(card) != tuple:
             raise TypeError("Error: Card is not a tuple")
@@ -61,12 +64,15 @@ class CardStack:
             raise ValueError("Error: Card is not valid")
         self.cards_on_stack[card[0]].append(card[1])
         self.cards_in_play[card[0]].remove(card[1])
+        return True
 
     def put_hand_back_on_stack(self, hand):
         if type(hand) != list:
             raise TypeError("Error: Hand is not a list")
         for card in hand:
             self.put_card_back_on_stack(card)
+        return True
+
     def remove_card_from_stack(self, card):
         if type(card) != tuple:
             raise TypeError("Error: Card is not a tuple")
@@ -77,25 +83,25 @@ class CardStack:
         if card[1] not in self.cards_on_stack[card[0]]:
             raise ValueError("Error: Card is not valid")
         self.cards_on_stack[card[0]].remove(card[1])
+        return True
 
     def remove_cards_from_stack(self, cards):
         if type(cards) != list:
             raise TypeError("Error: Cards is not a list")
         for card in cards:
-            remove_card_from_stack(card)
+            self.remove_card_from_stack(card)
+        return True
 
 
-
-
-
-def get_cards_in_play(Stack):
-    if type(Stack) != CardStack:
+def get_cards_in_play(stack):
+    if type(stack) != CardStack:
         raise TypeError("Error: Object is not a Card Stack")
     else:
-        return Stack.get_cards_in_play()
+        return stack.get_cards_in_play()
 
-def get_cards_on_stack(Stack):
-    if type(Stack) != CardStack:
+
+def get_cards_on_stack(stack):
+    if type(stack) != CardStack:
         raise TypeError("Error: Object is not a Card Stack")
     else:
-        return Stack.get_cards_on_stack()
+        return stack.get_cards_on_stack()
